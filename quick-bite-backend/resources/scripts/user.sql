810218e8f201d9ccb50d03ddc03a8c82e8442772
nidalJaafar <nidaljaafar.nj@gmail.com>
2022-04-29 01:21:54 +0300

    edited the sql script

--- quick-bite-backend/resources/scripts/user.sql
+++ quick-bite-backend/resources/scripts/user.sql
@@ -1,7 +1,8 @@
-DROP DATABASE IF EXISTS `quick_bite`;
-DROP USER IF EXISTS `quick_bite_admin`;
-
-CREATE DATABASE `quick_bite` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER `quick_bite_admin`@`localhost` IDENTIFIED WITH mysql_native_password BY 'password';
-
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW, EVENT, TRIGGER ON `quick_bite`.* TO `quick_bite_admin`@`localhost`;
+DROP DATABASE IF EXISTS quick_bite;
+DROP USER IF EXISTS `quick_bite_admin`@`%`;
+DROP USER IF EXISTS `quick_bite_admin`@`%`;
+CREATE DATABASE IF NOT EXISTS quick_bite CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `quick_bite_admin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW
+ ON `quick_bite`.* TO `quick_bite_admin`@`%`;
+FLUSH PRIVILEGES;
