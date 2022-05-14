319118cf017bc83bc83456667f25bc89c5ac7845
nidalJaafar <nidaljaafar.nj@gmail.com>
2022-05-14 20:52:14 +0300

    refactored the signup and login responses

---
+++ quick-bite-backend/app/Http/Resources/TokenResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TokenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'token' => $this->resource->token,
+            'user' => new UserResource($this->resource->user)
+        ];
+    }
+}
